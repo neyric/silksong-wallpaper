b5a1570fbef0bd8f890c07b82d41fd4590f84923
neyric <finegn@outlook.com>
2025-09-14 08:05:58 +0800

    Refactor HeroSection component to use Image component and clean up code
    - Removed PropsWithChildren and HeroSectionProps interface as they were unnecessary.
    - Replaced the <img> tag with the new <Image> component for better image handling.
    - Cleaned up the layout and formatting of the HeroSection content.

--- app/components/pages/landing/hero.tsx
+++ app/components/pages/landing/hero.tsx
@@ -1,28 +1,24 @@
-import type { PropsWithChildren } from "react";
-import { GridSection } from "~/components/ui/grid-section";
+import { Image } from "~/components/common";
 
-interface HeroSectionProps {
-  title: string;
-  description: string;
-}
-
-export function HeroSection({
-  title,
-  description,
-  children,
-}: PropsWithChildren<HeroSectionProps>) {
+export function HeroSection() {
   return (
     <div className="bg-black text-white">
       <div className="relative overflow-hidden container md:h-80 lg:h-120">
         <div className="max-md:w-full md:absolute md:-right-15 lg:-right-20 inset-y-0 pointer-events-none select-none">
-          <img className="h-full size-full object-cover" src="/assets/banner.webp" />
+          <Image
+            className="h-full size-full object-cover"
+            src="/assets/banner.webp"
+            alt="Hollow Knight: Silksong Banner"
+          />
         </div>
         <div className="relative h-full flex flex-col items-start justify-center max-md:py-6 md:max-w-80 lg:max-w-100 xl:max-w-120">
           <h1 className="text-2xl sm:text-3xl md:text-5xl lg:text-6xl font-medium font-logo select-none mb-2 sm:mb-4 lg:mb-6">
             Silksong <br className="hidden md:block" />
             Wallpaper
           </h1>
-          <p className="text-base sm:text-lg md:text-xl lg:text-2xl">Premium Hollow Knight: Silksong Wallpaper Collection</p>
+          <p className="text-base sm:text-lg md:text-xl lg:text-2xl">
+            Premium Hollow Knight: Silksong Wallpaper Collection
+          </p>
           <div className="mt-4 sm:mt-6 md:mt-8 lg:mt-12 flex items-center justify-center gap-6">
             <button className="btn btn-primary">Get Wallpapers</button>
             <p className="text-neutral-300">4K / 8K / Mobile</p>
